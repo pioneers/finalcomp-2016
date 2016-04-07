57477db51e69407fe7416b57739157751b8e350c
Daniel Jiang <daniel.lu.jiang@gmail.com>
2016-04-06 17:29:17 -0700

    Major overhaul

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -35,7 +35,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   app.addEventListener('dom-change', function() {
     console.log('Our app is ready to rock!');
   });
-  
+
   // See https://github.com/Polymer/polymer/issues/1381
   window.addEventListener('WebComponentsReady', function() {
     // imports are loaded and elements have been registered
