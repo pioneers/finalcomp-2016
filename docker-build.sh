03c55fe7e2de621944a68b7d1304630eae33f0b4
Daniel Jiang <daniel.lu.jiang@gmail.com>
2016-04-15 22:47:49 -0700

    Add variable names to docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,6 @@
+IMAGE_NAME=match-sched
+CONTAINER_NAME=finalcomp
 
-docker build -t match-sched .
-docker run -d --name finalcomp -p 5000:5000 match-sched
+# Find Dockerfile in current context and build an image of it, tagging the image "match-sched"
+docker build -t $IMAGE_NAME .
+docker run -d --name $CONTAINER_NAME -p 5000:5000 $IMAGE_NAME
