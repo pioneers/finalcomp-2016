f17398382e2e88f177fcc94198f358fc5551aba4
Daniel Jiang <daniel.lu.jiang@gmail.com>
2016-04-15 22:51:26 -0700

    Add comments to docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,14 @@
+# Sets image name as variable. Image name is arbitrary
 IMAGE_NAME=match-sched
+
+# Sets container name as variable. Container name is also arbitrary
 CONTAINER_NAME=finalcomp
 
-# Find Dockerfile in current context and build an image of it, tagging the image "match-sched"
+# Finds Dockerfile in current context and builds an image of it
+# Also tags the image with the name specified in the IMAGE_NAME variable
 docker build -t $IMAGE_NAME .
+
+# Runs a container off of the image as a daemon process
+# Also names the container the name specified in the CONTAINER_NAME variable
+# Also maps port 5000 of the container to port 5000 of the host
 docker run -d --name $CONTAINER_NAME -p 5000:5000 $IMAGE_NAME
